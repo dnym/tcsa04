e719163b9a2872e6c5d1ac67d15e0c5d57344f84
David Nyman <david.nyman@protonmail.com>
2023-10-11 16:42:27 +0200

    Implement memory storage

---
+++ CodingTracker/DataAccess/IDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using CodingTracker.Models;
+
+namespace CodingTracker.DataAccess;
+
+internal interface IDataAccess
+{
+    void Insert(CodingSession session);
+    CodingSession? Get(int id);
+    IEnumerable<CodingSession> GetAll();
+    void Update(CodingSession session);
+    void Delete(int id);
+}
---
+++ CodingTracker/DataAccess/MemoryStorage.cs
@@ -0,0 +1,52 @@
+﻿using CodingTracker.Models;
+
+namespace CodingTracker.DataAccess;
+
+internal class MemoryStorage : IDataAccess
+{
+    private readonly List<CodingSession> _sessions = new();
+
+    public void Insert(CodingSession session)
+    {
+        _sessions.Add(Clone(session));
+    }
+
+    public CodingSession? Get(int id)
+    {
+        var output = _sessions.Find(s => s.Id == id);
+        if (output != null)
+        {
+            output = Clone(output);
+        }
+        return output;
+    }
+
+    public IEnumerable<CodingSession> GetAll()
+    {
+        return _sessions.Select(Clone);
+    }
+
+    public void Update(CodingSession session)
+    {
+        var index = _sessions.FindIndex(s => s.Id == session.Id);
+        if (index != -1)
+        {
+            _sessions[index] = Clone(session);
+        }
+    }
+
+    public void Delete(int id)
+    {
+        _sessions.RemoveAll(s => s.Id == id);
+    }
+
+    private static CodingSession Clone(CodingSession session)
+    {
+        return new CodingSession
+        {
+            Id = session.Id,
+            StartTime = session.StartTime,
+            EndTime = session.EndTime
+        };
+    }
+}
