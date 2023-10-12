577657da368480bb4ecdbe6f80653878b66c47ed
David Nyman <david.nyman@protonmail.com>
2023-10-12 12:22:52 +0200

    Add pagination

--- CodingTracker/UI/LogManagementScreen.cs
+++ CodingTracker/UI/LogManagementScreen.cs
@@ -8,18 +8,62 @@ internal static class LogManagementScreen
 {
     internal static Screen Get(IDataAccess dataAccess)
     {
+        int skip = 0;
+        int lastTaken = 0;
+
         static string header(int _1, int _2)
         {
             return "Coding Sessions";
         }
 
-        string body(int _1, int _2)
+        string footer(int _, int usableHeight)
         {
-            return MakeListString(dataAccess);
+            const string escHint = "[Esc] to go back to the main menu.";
+            var footerHeight = 1 + 2;// +2 due to bar separator and empty line.
+            var output = "Press ";
+            if (skip > 0)
+            {
+                output += "[PgUp] to go to the previous page,\n";
+                footerHeight++;
+            }
+            if (dataAccess.Count() - skip > (usableHeight - footerHeight))
+            {
+                output += "[PgDown] to go to the next page,\n";
+            }
+            if (skip > 0 || dataAccess.Count() - skip > usableHeight)
+            {
+                output += "or ";
+            }
+            output += escHint;
+            return output;
         }
 
-        var screen = new Screen(header: header, body: body);
+        string body(int _, int usableHeight)
+        {
+            lastTaken = usableHeight;
+            return MakeListString(dataAccess, skip, usableHeight);
+        }
+
+        void pgUp()
+        {
+            if (skip > 0)
+            {
+                skip = Math.Max(0 , skip - lastTaken);
+            }
+        }
+
+        void pgDown()
+        {
+            if (dataAccess.Count() - skip > lastTaken)
+            {
+                skip += lastTaken;
+            }
+        }
+
+        var screen = new Screen(header: header, body: body, footer: footer);
         screen.AddAction(ConsoleKey.D0, () => screen.ExitScreen());
+        screen.AddAction(ConsoleKey.PageUp, pgUp);
+        screen.AddAction(ConsoleKey.PageDown, pgDown);
         return screen;
     }
 
