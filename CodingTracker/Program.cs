235ca814b706937ce8daf259b21a0db589b26a87
David Nyman <david.nyman@protonmail.com>
2023-10-12 10:01:14 +0200

    Add dummy data generation

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CodingTracker.DataAccess;
+using CodingTracker.Models;
 
 namespace CodingTracker;
 
@@ -7,7 +8,33 @@ internal static class Program
     static void Main()
     {
         IDataAccess dataAccess = new MemoryStorage();
+        InsertDummySessions(dataAccess, 50);
         UI.MainMenu.Get(dataAccess).Show();
         Console.Clear();
+    }
+
+    private static void InsertDummySessions(IDataAccess dataAccess, int n)
+    {
+        var random = new Random();
+        var months = (int)Math.Ceiling(n / 30d);
+        var earliestStartDate = DateTime.UtcNow.AddMonths(-months).Ticks;
+        var greatestDiff = DateTime.UtcNow.Ticks - earliestStartDate;
+        var shortestSessionLength = TimeSpan.FromMinutes(5).Ticks;
+        var longestSessionLength = TimeSpan.FromHours(8).Ticks;
+        while (dataAccess.Count() < n)
+        {
+            var startDate = new DateTime(earliestStartDate + (long)(random.NextDouble() * greatestDiff));
+            var randomDuration = new TimeSpan((long)(random.NextDouble() * (longestSessionLength - shortestSessionLength)) + shortestSessionLength);
+            var endDate = startDate + randomDuration;
+            var session = new CodingSession()
+            {
+                StartTime = startDate,
+                EndTime = endDate,
+            };
+            if (!dataAccess.CheckForOverlap(session).Any())
+            {
+                dataAccess.Insert(session);
+            }
+        }
     }
 }
