4f45d13452dcdc9ebc34921701d7be8cd49a7833
David Nyman <david.nyman@protonmail.com>
2023-10-13 15:28:59 +0200

    Add SQLite access

---
+++ CodingTracker/DataAccess/SqliteStorage.cs
@@ -0,0 +1,48 @@
+﻿using CodingTracker.Models;
+
+namespace CodingTracker.DataAccess;
+
+internal class SqliteStorage : IDataAccess
+{
+    private readonly string _connectionString;
+
+    public SqliteStorage(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public void Insert(CodingSession session)
+    {
+        throw new NotImplementedException();
+    }
+
+    public CodingSession? Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IList<CodingSession> GetAll(IDataAccess.Order order = IDataAccess.Order.Ascending, int skip = 0, int limit = int.MaxValue)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Count()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(CodingSession session)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IList<CodingSession> CheckForOverlap(CodingSession session)
+    {
+        throw new NotImplementedException();
+    }
+}
