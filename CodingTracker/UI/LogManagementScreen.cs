351562b065b411ccdd2624e45f3cd5cb592583fb
David Nyman <david.nyman@protonmail.com>
2023-10-16 08:22:52 +0200

    Improve empty session list handling

--- CodingTracker/UI/LogManagementScreen.cs
+++ CodingTracker/UI/LogManagementScreen.cs
@@ -62,6 +62,10 @@ internal static class LogManagementScreen
 
         string body(int _1, int _2)
         {
+            if (dataAccess.Count() == 0)
+            {
+                return "There are no logged coding sessions yet.";
+            }
             // TODO: Caching? Currently, we're getting all sessions every time we refresh the screen, which is suboptimal (but no problem with SQLite). But if we cache, we need to invalidate the cache when the user adds, modifies, or deletes a session.
             var sessions = dataAccess.GetAll(skip: skip, limit: ListItemsPerPage(listUsableHeight)).ToList();
             listNumbersToIds = sessions.ConvertAll(cs => cs.Id).ToArray();
@@ -85,6 +89,8 @@ internal static class LogManagementScreen
             }
         }
 
+        var screen = new Screen(header: header, body: body, footer: footer);
+
         void handleUserInput(string text)
         {
             if (int.TryParse(text, out int result) && result >= 1 && result <= listNumbersToIds.Length)
@@ -96,6 +102,10 @@ internal static class LogManagementScreen
                 if (dataAccess.Count() - skip <= 0)
                 {
                     skip = Math.Max(0, skip - ListItemsPerPage(listUsableHeight));
+                    if (dataAccess.Count() == 0)
+                    {
+                        screen.SetPromptAction(null);
+                    }
                 }
             }
             else
@@ -103,12 +113,13 @@ internal static class LogManagementScreen
                 System.Console.Beep();
             }
         }
-
-        var screen = new Screen(header: header, body: body, footer: footer);
         screen.AddAction(ConsoleKey.Escape, () => screen.ExitScreen());
         screen.AddAction(ConsoleKey.PageUp, pgUp);
         screen.AddAction(ConsoleKey.PageDown, pgDown);
-        screen.SetPromptAction(handleUserInput);
+        if (dataAccess.Count() > 0)
+        {
+            screen.SetPromptAction(handleUserInput);
+        }
         return screen;
     }
 
