648cbc9e171d1c734580732c48bce8110843abf8
David Nyman <david.nyman@protonmail.com>
2023-10-16 08:33:53 +0200

    Remove dummy data generation

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,5 +1,4 @@
 ﻿using CodingTracker.DataAccess;
-using CodingTracker.Models;
 using System.Configuration;
 
 namespace CodingTracker;
@@ -12,37 +11,7 @@ internal static class Program
     {
         string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString") ?? _defaultConnectionString;
         IDataAccess dataAccess = new SqliteStorage(connectionString);
-        InsertDummySessions(dataAccess, 50);
         UI.MainMenu.Get(dataAccess).Show();
         Console.Clear();
-    }
-
-    private static void InsertDummySessions(IDataAccess dataAccess, int n)
-    {
-        var random = new Random();
-        var months = (int)Math.Ceiling(n / 30d);
-        var earliestStartDate = DateTime.UtcNow.AddMonths(-months).Ticks;
-        var greatestDiff = DateTime.UtcNow.Ticks - earliestStartDate;
-        var shortestSessionLength = TimeSpan.FromMinutes(5).Ticks;
-        var longestSessionLength = TimeSpan.FromHours(8).Ticks;
-        while (dataAccess.Count() < n)
-        {
-            var startDate = new DateTime(earliestStartDate + (long)(random.NextDouble() * greatestDiff));
-            var randomDuration = new TimeSpan((long)(random.NextDouble() * (longestSessionLength - shortestSessionLength)) + shortestSessionLength);
-            var endDate = startDate + randomDuration;
-            if (endDate > DateTime.UtcNow)
-            {
-                endDate = DateTime.UtcNow;
-            }
-            var session = new CodingSession()
-            {
-                StartTime = startDate,
-                EndTime = endDate,
-            };
-            if (!dataAccess.CheckForOverlap(session).Any())
-            {
-                dataAccess.Insert(session);
-            }
-        }
     }
 }
